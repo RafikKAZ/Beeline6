575c4aefe8e61e808f61a5f1420c735ea8b314a3
RafikKAZ <rafikazer89@gmail.com>
2025-05-21 19:07:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const formData = new FormData(event.target);
 
         try {
-            const response = await fetch("https://script.google.com/macros/s/AKfycbwXwKbErq1iK2XoWCh8R5EeyEs9IBYG7CA47Z-_J2UpvS6YB5GoJjaEK3x46OynLoqXiw/exec", {
+            const response = await fetch("https://script.google.com/macros/s/AKfycbz991pKsrmTXf-i_vg9zbz-RtxfRMAMFw4J729FfL5xkFe4m0e6mi2H9GLKYCidVvxQwQ/exec", {
                 method: "POST",
                 body: formData,
             });
